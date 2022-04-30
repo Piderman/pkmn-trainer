79ce55e544cdd04e6b66d15419ec40803c408ae1
Matty <matty@mattycollins.com.au>
2022-05-01 01:17:26 +1000

    CORS headers NFI

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -6,12 +6,16 @@ const handler = async (event) => {
   try {
     const response = await axios.$get('https://api.printful.com/products', {
       headers: {
+        "Access-Control-Allow-Origin": "*",
         "X-PF-Store-Id": process.env.PRINTFUL_STORE_ID,
         Authorization: `Basic ${process.env.PRINTFUL_TOKEN}`
       }
     }).catch((error) => {
       console.log('[product] await error', error)
-      return { statusCode: 500, body: error.toString() }
+      return {
+        statusCode: 500,
+        body: error.toString()
+      }
     })
 
 
